f6928737e940cb17ac61eab255e23df28dcd012c
oliver-puppio <68809155+oliver-puppio@users.noreply.github.com>
2021-06-07 17:22:37 +0800

    增加了ashx

---
+++ Word词频分析/Analyzer.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Word词频分析
+{
+    /// <summary>
+    /// Analyzer 的摘要说明
+    /// </summary>
+    public class Analyzer : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
