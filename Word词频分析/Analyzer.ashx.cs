a39285ef2f18ce7c23e2cb7519eb08bbea812177
oliver-puppio <68809155+oliver-puppio@users.noreply.github.com>
2021-06-08 20:53:47 +0800

    重大更新

--- Word词频分析/Analyzer.ashx.cs
+++ Word词频分析/Analyzer.ashx.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
+using System.Text;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using System.Threading.Tasks;
+using JiebaNet.Segmenter;
 
 namespace Word词频分析
 {
@@ -13,10 +19,41 @@ namespace Word词频分析
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";
-            context.Response.Write("Hello World");
-        }
+            if (context.Request.Files.Count > 0)
+            {               
+                //获取传送的文件,并保存在library中
+                context.Response.ContentType = "text/plain";             
+                //HttpPostedFile f = context.Request.Files[0];
+                HttpPostedFile f = HttpContext.Current.Request.Files[0];
+                f.SaveAs(HttpContext.Current.Server.MapPath("Library/" + f.FileName));
+                string path = HttpContext.Current.Server.MapPath("~/Library/") +f.FileName;
 
+                //读取word文件
+                Document doc = new Document();
+                doc.LoadFromFile(path);
+                string s = doc.GetText();
+                File.WriteAllText(HttpContext.Current.Server.MapPath("~/Library/tempt.txt"), s.ToString());
+                string temp = HttpContext.Current.Server.MapPath("~/Library/tempt.txt");
+
+                //读取txt文件
+                string[] txt_str = File.ReadAllLines(temp, Encoding.UTF8);
+                string str="";
+                foreach (string i in txt_str)
+                {
+                    str+=i;
+                }
+
+                //处理词频
+                var segmenter = new JiebaSegmenter();                
+                var segments2 = segmenter.Cut(str);
+                context.Response.Write(segments2);
+            }
+
+            else
+                context.Response.Write("No call");
+            context.Response.End();
+        }
+        
         public bool IsReusable
         {
             get
@@ -24,5 +61,53 @@ namespace Word词频分析
                 return false;
             }
         }
+
+        static void Test_Main(string[] args)
+        {
+            var segmenter = new JiebaSegmenter();
+            //var segments2 = segmenter.Cut("我来到北京清华大学我来到北京北京清华大学");
+            //获取待分析的text字符串
+            string text = "我来到北京清华大学我来到北京北京清华大学";
+            //分词
+            var segments2 = segmenter.Cut(text);
+            Console.WriteLine("【精确模式】：{0}", string.Join("/ ", segments2));
+
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            //遍历segments列表
+            for (int i = 0; i < segments2.Count(); i++)
+            {
+                var tmpstr = segments2.ElementAt(i);
+                if (!dic.ContainsKey(tmpstr))
+                {
+                    dic.Add(tmpstr, 1);
+                }
+                else
+                {
+                    dic[tmpstr]++;
+                }
+            }
+
+            //Console.WriteLine("dic是：{");
+            ////遍历字典            
+            //foreach(KeyValuePair<string,int> kvp in dic)
+            //{
+            //    Console.WriteLine(kvp.Key + ":" + kvp.Value);
+            //}
+            //Console.WriteLine("}");
+
+            //字典类型用上面的方法遍历访问key和value。
+            string wnf = "{";
+            foreach (KeyValuePair<string, int> kvp in dic)
+            {
+                wnf += kvp.Key + ":" + kvp.Value + ";";
+            }
+            wnf += "}";
+            Console.WriteLine(wnf);
+
+            Console.ReadLine();
+        }
     }
 }
+
+
+
--- Word词频分析/Backend.js
+++ Word词频分析/Backend.js
@@ -1,1 +1,60 @@
 ﻿
+function Ajax(method, url, param, callback) {
+    method = method || "get";/* 设置method属性默认值get*/
+    param = param || null;
+
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+    xhr.open(method, url, true);//open方法只是设置了一些请求参数。
+
+    //如果请求方法是post,下面这名必须加
+    if (method.toLowerCase() == "post") {
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    }
+
+    xhr.send(param);//发送请求及数据，
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+
+                //使用回调函数
+                if (callback) {
+                    callback(xhr.responseText);
+                }
+                else {
+                    document.write(xhr.responseText);
+                }
+            }
+        }
+    }
+}
+
+//$("#按钮").click(function () {
+//    var files = $('#Doc_Upload').prop('files');
+//    var data = new FormData();
+//    data.append('Doc_Upload', files[0]);
+
+//    $.ajax({
+//        type: 'POST',
+//        url: "Analyzer.ashx",
+//        data: data,
+//        cache: false,
+//        processData: false,
+//        contentType: false,
+//        success: function (ret) {
+//            alert(ret);
+//        }
+//    });
+//});
+function show_in_div(param) {
+    document.getElementById('div1').innerText = " <div id='div1'>"+param+"</div>";
+}
+
+function Upload() {
+    var files = document.getElementById("Doc_Upload").prop("files");
+    var data = new FormData();
+    data.append('Doc_Upload', files[0]);
+
+    Ajax("post", "Analyzer.ashx.cs",data,show_in_div);
+}
